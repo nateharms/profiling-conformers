e01346df8f7672e955a5fe30439be822fab851dc
nateharms <harms.n@husky.neu.edu>
2019-09-03 10:53:17 -0400

    got results for species optimization

---
+++ profile.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#set a job name
+#SBATCH --job-name=profiling-conformers
+
+#a file for job output, you can check job progress
+#SBATCH --output=conformers.log
+#SBATCH --error=conformers.log
+
+
+#SBATCH -N 1
+#SBATCH -n 2
+##SBATCH --ntasks-per-node=12
+#SBATCH --partition=general
+#SBATCH --time=24:00:00
+#SBATCH --mem=5GB
+#SBATCH --exclude=c3040,c5003
+source $HOME/.bashrc
+conda activate rmg_env
+python profile.py
